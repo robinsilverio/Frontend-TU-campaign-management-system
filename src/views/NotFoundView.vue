49c0a87899d071d5d9f943f425c04f4c4d041d89
Robin Medeiros Silvério <r.medeiros.silverio@technischeunie.com>
2023-07-08 17:54:44 +0200

    Trigger 404-page redirection.

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,18 @@
+<script setup>
+    import NavigationBar from '../components/NavigationBar.vue';
+</script>
+<template>
+    <NavigationBar></NavigationBar>
+    <main class="not-found">
+        <h1>404 - page not found</h1>
+    </main>
+</template>
+<script>
+    export default {}
+</script>
+<style scoped>
+    main.not-found {
+        height: 100vh;
+        padding: 10px;
+    }
+</style>
