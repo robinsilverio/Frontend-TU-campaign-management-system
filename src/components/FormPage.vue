05958532a4c7d42bd0b77dc603fb27b86b83d201
Robin Medeiros Silvério <r.medeiros.silverio@technischeunie.com>
2023-07-25 10:14:30 +0200

    First part of form validation is done by validating forms of tabs Basic and advanced. Also wrote a map functions for triggering datamutation process based on user actions.

--- src/components/FormPage.vue
+++ src/components/FormPage.vue
@@ -10,12 +10,23 @@
     <div class="form-body">
       <h1>{{ this.capitalizedActiveTab }}</h1>
       <form class="campaign-form" name="campaign-form" @submit.prevent="formSubmit">
-        <div :class="'form-control input-' + tabForm.name" v-for="(tabForm, index) in this.tabForms[activeTab].inputFields" :key="index">
-          <label for="{{tabForm.name}}">{{tabForm.label}}</label>
-          <input :type="tabForm.type" :name="tabForm.name" :value="tabForm.value" v-if="tabForm.type !== 'textarea'" />
-          <textarea name="{{tabForm.name}}" v-if="tabForm.type === 'textarea'"></textarea>
+        <div class="form-controls">
+          <div :class="'form-control input-' + tabForm.name" v-for="(tabForm, index) in this.tabForms[activeTab].inputFields" :key="index">
+            <label for="{{tabForm.name}}">{{tabForm.label}}</label>
+            <input :type="tabForm.type" :name="tabForm.name" v-model="tabForm.value" v-if="tabForm.type !== 'textarea' && tabForm.type !== 'selectbox' && tabForm.type !== 'radiogroup'" />
+            <div class="radio-wrapper" v-if="tabForm.type === 'radiogroup'">
+              <div v-for="(radioOption, subIndex) in tabForm.radioOptions" :key="subIndex">
+                <input type="radio" :id="`${tabForm.name}-${subIndex}`" :value="radioOption.value" v-model="tabForm.value" />
+                <label :for="`${tabForm.name}-${subIndex}`">{{ radioOption.label }}</label>
+              </div>
+            </div>
+            <select :name="tabForm.name" v-model="tabForm.value" v-if="tabForm.type !== 'radiogroup' && tabForm.type === 'selectbox'">
+              <option v-for="(option, subIndex) in tabForm.options" :key="subIndex" :value="option">{{ option }}</option>
+            </select>
+            <textarea name="{{tabForm.name}}" v-if="tabForm.type === 'textarea'" v-model="tabForm.value"></textarea>
+          </div>
         </div>
-        <div class="form-control">
+        <div class="form-actions">
           <button class="form-submit">{{ this.capitalizedUserAction }} campaign</button>
         </div>
       </form>
@@ -31,35 +42,62 @@ export default {
       tabForms: {
         "basic" : {
             inputFields: [
-              { type: 'text', name: 'title', label: 'Title: ', value: null },
-              { type: 'text', name: 'campaign-url', label: 'Campaign url: ', value: null },
-              { type: 'date', name: 'start-date', label: 'Start date: ', value: null },
-              { type: 'date', name: 'end-date', label: 'End date: ', value: null },
-              { type: 'textarea', name: 'promo-description', label: 'Campaign Promotional description Text: ', value: '' },
-              { type: 'textarea', name: 'promo-summary', label: 'Campaign Promotional Summary Text: ', value: '' },
-              { type: 'text', name: 'ribbon-text', label: 'Ribbon Text: ', value: '' },
-              { type: 'text', name: 'campaign-terms-url: ', label: 'Campaign Terms Url: ', value: '' }
+              { type: 'text', name: 'title', label: 'Title: ', required: true, value: null },
+              { type: 'text', name: 'campaign-url', label: 'Campaign url: ', required: false, value: null },
+              { type: 'date', name: 'start-date', label: 'Start date: ', required: true, value: null },
+              { type: 'date', name: 'end-date', label: 'End date: ', required: true, value: null },
+              { type: 'textarea', name: 'promo-description', label: 'Campaign Promotional description Text: ', required: true, value: null },
+              { type: 'textarea', name: 'promo-summary', label: 'Campaign Promotional Summary Text: ', required: true, value: null },
+              { type: 'text', name: 'ribbon-text', label: 'Ribbon Text: ', required: true, value: null },
+              { type: 'text', name: 'campaign-terms-url: ', label: 'Campaign Terms Url: ', required: false, value: null }
             ]
         },
         "advanced" : {
-          inputFields: [],
+          inputFields: [
+            {
+              type: 'selectbox',
+              name: 'client-group',
+              label: 'Client Group(s): ',
+              options: [
+                "ADVIES",
+                "REST",
+                "INSTG-TOT",
+                "INSTG-SANCV",
+                "INSTK-SANCV",
+                "INSTK-TOT",
+                "RET-SHW",
+                "LEV",
+                "INSTG-ET",
+                "RET-OV",
+                "S&O",
+                "IND",
+                "ONBEKEND",
+                "INSTK-ET",
+                "MEDEWERKER",
+              ],
+              required: true,
+              value: null },
+            {
+              type: 'radiogroup',
+              name: 'root-indicator',
+              label: 'Root Indicator: ',
+              radioOptions: [
+                {label: 'Ja', value: 1},
+                {label: 'Nee', value: 0}
+              ],
+              required: false,
+              value: 1
+            }
+          ],
         },
         "images" : {
-          inputFields: []
         },
         "campaign-items" : {
-          inputFields: [
-            { type: 'text', name: 'tag', label: '', value: '' },
-          ],
-          savedCampaignItems: []
         },
         "tags" : {
-          inputFields: [
-            { type: 'text', name: 'tag', label: '', value: '' },
-          ],
-          savedTags: []
         }
-      }
+      },
+      errorMessages: []
     }
   },
   computed: {
@@ -83,11 +121,34 @@ export default {
       this.activeTab = paramTabName;
     },
     formSubmit() {
-      if (this.userAction === 'create') {
-        console.log('Perform create process.')
-      } else {
-        console.log('Perform update process.')
+      this.validateInput();
+      if (this.errorMessages.length > 0) {
+        this.errorMessages.forEach(errorMessage => this.$toast.error(errorMessage));
+        this.errorMessages = [];
+        return;
       }
+      this.mapFunctions()[this.userAction]();
+    },
+    mapFunctions() {
+      return {
+        "create": () =>{
+          this.$toast.success("Een campagne is succesvol toegevoegd.")
+        },
+        "update": () => {
+          this.$toast.success("Een campagne is succesvol bijgewerkt.");
+        }
+      }
+    },
+    validateInput() {
+      Object.keys(this.tabForms).forEach((key) => {
+        console.log(key);
+
+        this.tabForms[key].inputFields && this.tabForms[key].inputFields.forEach((field) => {
+          if ((field.value === null || field.value === '') && field.required) {
+            this.errorMessages.push(`Er ontbreekt een waarde voor de vereiste eigenschap "${field.label}"`);
+          }
+        });
+      });
     }
   }
 }
@@ -95,10 +156,9 @@ export default {
 <style scoped>
   .form-page {
     width: 80%;
-    height: 600px;
     margin: auto;
     position: relative;
-    top: 10%;
+    top: 1%;
   }
   .form-page .tab-header {
     display: flex;
@@ -121,11 +181,21 @@ export default {
   }
   .form-page .form-body {
     background-color: #FFF;
-    height: 100%;
     border: 1px solid;
     padding: 20px;
   }
-  .form-page .form-body .campaign-form {
-    margin-top: 25px;
+  .form-page .form-body form .form-controls .form-control {
+    display: flex;
+    flex-direction: column;
+  }
+  .form-control textarea {
+    height: 150px;
+  }
+  .form-control .radio-wrapper {
+    display: flex;
+    gap: 10px;
+  }
+  .form-actions {
+    margin-top: 30px;
   }
 </style>
